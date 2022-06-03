5a44bdcd4bf24757f999986dab065ffd793c4620
Robyn Drake <robin.drake.041@gmail.com>
2022-06-03 02:50:34 -0500

    Changed app.js to use the static files we added

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 var createError = require('http-errors');
 var express = require('express');
+const bodyParser = require("body-parser");
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var wikiRouter = require('./routes/wiki');
 
 var app = express();
@@ -19,10 +18,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/about', wikiRouter);
+app.get('/', (req, res)  => {
+	res.sendFile(__dirname + "/static/html/index.html");
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
